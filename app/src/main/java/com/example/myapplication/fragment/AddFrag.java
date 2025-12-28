29d1669181d5604fef9f9edc8782599da7d4d543
Norak-PNG <iuuu5359@gmail.com>
2025-12-28 18:12:08 +0700

    image loading with Glide

--- app/src/main/java/com/example/myapplication/fragment/AddFrag.java
+++ app/src/main/java/com/example/myapplication/fragment/AddFrag.java
@@ -2,9 +2,7 @@ package com.example.myapplication.fragment;
 
 import static android.app.Activity.RESULT_OK;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -25,7 +23,6 @@ import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
 import com.example.myapplication.CameraActivity;
-import com.example.myapplication.MainActivity;
 import com.example.myapplication.database.JsonPlaceholderApi;
 import com.example.myapplication.NewCategoryActivity;
 import com.example.myapplication.database.Post;
